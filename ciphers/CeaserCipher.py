3965ab56609273a15c9a8974ce6b4486be5da454
madrajib <madhabsharma94@gmail.com>
2022-11-23 22:10:33 +0530

    Add Ceaser Cipher code

--- ciphers/CeaserCipher.py
+++ ciphers/CeaserCipher.py
@@ -2,16 +2,35 @@ class CeaserCipher:
     def __init__(self) -> None:
         pass
 
-    def encrypt(self, text, key):
-        return text
+    def encrypt(self, text, key):  
+        text =text.replace(" ","")
+        result=""  #empty string
+        for i in range(len(text)):
+            char=text[i]
+            if(char.isupper()):  #if the text[i] is in upper case
+                result=result+chr((ord(char)+key-65)%26+65)
+            else:
+                result=result+chr((ord(char)+key-97)%26+97)
+        return result
 
-    def decrypt(self,text):
-        return text
+    def decrypt(self,text, key):
+        # Cipher(n) = De-cipher(26-n)
+        key = 26-key 
+        
+        result=""  #empty string
+        for i in range(len(text)):
+            char=text[i]
+            if(char.isupper()):  #if the text[i] is in upper case
+                result=result+chr((ord(char)+key-65)%26+65)
+            else:
+                result=result+chr((ord(char)+key-97)%26+97)
+        return result
     
     def process_request(self, text, option, key=None):
         print(text, option,key)
         option = int(option)
+        key = int(key)
         if (option == 1 and key != None):
             return self.encrypt(text, key)
         elif (option == 2):
-            return self.decrypt(text)
+            return self.decrypt(text, key)
